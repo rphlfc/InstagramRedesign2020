6d1153d450831bd589dce1797d6557224cd9c4ff
Raphael Cerqueira <rcerqueira@vr.com.br>
2020-09-16 20:08:37 -0300

    Initial Commit

---
+++ InstagramRedesign2020/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InstagramRedesign2020
+//
+//  Created by Raphael Cerqueira on 16/09/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
